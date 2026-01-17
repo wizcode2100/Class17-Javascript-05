6a99d4241228d3d048f5de12219b0cb9d0d8c45a
wizcode2100 <tauhid2100@gmail.com>
2026-01-17 03:06:32 -0500

    Javascript05

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+let marks1 = 40;
+let marks2 = 90;
+let marks3 = 32;
+
+let sum = marks1+marks2;
+
+console.log(marks3);
+console.log(sum);
+
+let marks = [40, 90, 32, 67];
+
+marks[1];
+marks[0];
+
+let arraysum = marks[2]+marks[3];
+console.log(marks[1]); //90
+console.log(arraysum);
+
+let name = "Rahim";
+let age = 32;
+let blood = "A+";
+let phone = "+8801XXXXXXXX";
+
+let person = {name: 'Rahim', age: 32, blood: 'A+', phone: '01987564222'};
+
+person.phone
+person.name
+
+console.log(person.phone);
+console.log(person.name);
+
+let matrix = [
+
+    [4, 9 , 10, 90, 0], 
+    [10, 20, 11, 2, 6],
+    [3, 4, 7, 0, 1],
+    [2, 5, 13, 9, 14]
+];
+
+matrix[1][2];
+matrix[2][2];
+
+console.log(matrix[1][2]);
